e960cb59ad03af52f9005031dadf0adf1ed3be40
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-07-22 03:02:31 +0600

    local data load done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
+import Watch from './components/Watch/Watch'
 
 function App() {
+  // local data load
+  const watches = [
+    { id: 1, name: 'Apple Watch', price: '49040', },
+    { id: 2, name: 'Samsung Watch', price: '29005', },
+    { id: 3, name: 'Oppo Watch', price: '9040', },
+    { id: 4, name: 'Nokia Watch', price: '4050', },
+    { id: 5, name: 'Haw Watch', price: '3050', },
+  ]
 
   return (
     <>
      
-      <h1>Vite + React</h1>
+      <h2>Watches: {watches.length} </h2>
+      {
+        watches.map(watch=> <Watch watch={watch}></Watch>)
+      }
       
       
     </>
---
+++ src/components/Watch/Watch.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Watch = ({ watch }) => {
+    const { id, name, price } = watch;
+    return (
+        <div>
+           
+            <small>Id: { id}</small>
+            <p>Name:{name}</p>
+            <small>Price:{price}</small>
+        </div>
+    );
+};
+
+export default Watch;
