25f567159e751a34ad56563913c4668a4d6d4e09
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-07-22 03:26:13 +0600

    Create and Load Fake data, chaptgpt json, online api  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Watch from './components/Watch/Watch'
 
 function App() {
   // local data load
-  const watches = [
-    { id: 1, name: 'Apple Watch', price: '49040', },
-    { id: 2, name: 'Samsung Watch', price: '29005', },
-    { id: 3, name: 'Oppo Watch', price: '9040', },
-    { id: 4, name: 'Nokia Watch', price: '4050', },
-    { id: 5, name: 'Haw Watch', price: '3050', },
-  ]
+  // const watches = [
+  //   { id: 1, name: "Amazfit Bip U Pro", price: 4999 },
+  //   { id: 2, name: "Samsung Galaxy Watch 6", price: 18999 },
+  //   { id: 3, name: "Apple Watch SE (2nd Gen)", price: 28999 },
+  //   { id: 4, name: "Noise ColorFit Ultra 3", price: 3499 },
+  //   { id: 5, name: "boAt Xtend Pro", price: 2999 }
+  // ]
+
+
+
+  // Json format data use with fetch from public watches.json file
+
+  // state for hold data
+  const [watches, setWatches] = useState([]);
+
+  // load data from watches.json file
+  useEffect(() => {
+    fetch('watches.json')
+      .then(res => res.json())
+      .then(data => setWatches(data))
+  },[])
+
 
   return (
     <>
